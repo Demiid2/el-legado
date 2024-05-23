b9c30988550975087db142c70456684b5ebcc966
DemiiD2 <demiandev2@gmail.com>
2024-05-23 18:43:46 -0300

    Inicio de el Legado

---
+++ src/lib/components/Header.svelte
@@ -0,0 +1,22 @@
+<ul class="header">
+    <li><a href="/">Inicio</a></li>
+    <li><a href="/Nosotros">Nostros</a></li>
+    <li><a href="/Contacto">Contacto</a></li>
+    <li><a href="/Cotizar">Cotizar</a></li>
+</ul>
+
+<style>
+    .header{
+        list-style: none;
+        display: flex;
+        flex-direction: row;
+    }
+    .header li{
+       padding-left: 30px;
+       font-size: large;
+    }
+    .header li:first-child{
+        font-size: larger;
+        font-weight: bold;
+    }
+</style>
